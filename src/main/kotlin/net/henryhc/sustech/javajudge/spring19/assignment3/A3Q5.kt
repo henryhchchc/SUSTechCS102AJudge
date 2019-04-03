288791cebfd593290e7269a8d8d4e72b062ccc69
Henry Chu <henrytech@outlook.com>
2019-04-04 00:24:29 +0800

    fix data

--- src/main/kotlin/net/henryhc/sustech/javajudge/spring19/assignment3/A3Q5.kt
+++ src/main/kotlin/net/henryhc/sustech/javajudge/spring19/assignment3/A3Q5.kt
@@ -13,7 +13,6 @@ private val m1 = arrayOf(
         arrayOf(0, 0, 0, 0, 1, 0),
         arrayOf(0, 1, 1, 1, 0, 0),
         arrayOf(0, 1, 1, 1, 0, 0),
-        arrayOf(0, 1, 1, 1, 0, 0),
         arrayOf(0, 1, 1, 1, 0, 0)
 )
 
