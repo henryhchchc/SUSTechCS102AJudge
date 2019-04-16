ba943267d5dd7c30869c050f214731cac0e7de3e
Henry Chu <henrytech@outlook.com>
2019-04-16 18:06:27 +0800

    add main class manifest for jar

--- src/main/kotlin/net/henryhc/sustech/javajudge/Program.kt
+++ src/main/kotlin/net/henryhc/sustech/javajudge/Program.kt
@@ -2,16 +2,22 @@ package net.henryhc.sustech.javajudge
 
 import net.henryhc.sustech.javajudge.sakai.Assignment
 import net.henryhc.sustech.javajudge.spring19.assignment3.problemsAssignment3
+import kotlin.system.exitProcess
 
 
 fun main(args: Array<String>) {
+    if (args.size != 1) {
+        System.err.println("Usage: java -jar <jar-file-name> <sakai-directory>")
+        exitProcess(1)
+    }
     val ass = Assignment(args[0], problemsAssignment3)
     val result = ass.judge()
     ass.writeGradeCsv(result.map { it.value })
     result.forEach { t, u ->
         t.writeFeedback(u.feedback)
-        println(t.student.name)
-        println(u.score)
-        println(u.feedback)
+//        println(t.student.name)
+//        println(u.score)
+//        println(u.feedback)
     }
+    println("Done")
 }
