58fa916681d73520f25ef635d05d0ec3a0a9968f
Henry Chu <henrytech@outlook.com>
2019-05-21 00:11:40 +0800

    refactorings

--- src/main/kotlin/net/henryhc/sustech/javajudge/testcases/unittest/JUnitTestCase.kt
+++ src/main/kotlin/net/henryhc/sustech/javajudge/testcases/unittest/JUnitTestCase.kt
@@ -4,6 +4,7 @@ import net.henryhc.sustech.javajudge.sakai.Submission
 import net.henryhc.sustech.javajudge.sakai.TestCaseJudgeResult
 import net.henryhc.sustech.javajudge.testcases.TestCase
 import org.junit.platform.engine.discovery.DiscoverySelectors.selectClass
+import org.junit.platform.launcher.core.LauncherConfig
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder
 import org.junit.platform.launcher.core.LauncherFactory
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener
@@ -15,19 +16,21 @@ class JUnitTestCase(
         val testClassName: String
 ) : TestCase(name) {
     override fun judge(submission: Submission): TestCaseJudgeResult {
-        val clazz = submission.attachmentsDirectory.toURI().toURL()
+        val launcher = LauncherFactory.create()
+        val summary = submission.attachmentsDirectory.toURI().toURL()
                 .let { URLClassLoader(arrayOf(it)) }
                 .loadClass(testClassName)
-        val request = LauncherDiscoveryRequestBuilder.request()
-                .selectors(selectClass(clazz))
-                .build()
-        val launcher = LauncherFactory.create()
-        val testPlan = launcher.discover(request)
-        val listener = SummaryGeneratingListener()
-        launcher.registerTestExecutionListeners(listener)
-
-        launcher.execute(testPlan)
-        val summary = listener.summary
+                .let {
+                    LauncherDiscoveryRequestBuilder.request()
+                            .selectors(selectClass(it))
+                            .build()
+                }
+                .let { launcher.discover(it) }
+                .let {
+                    SummaryGeneratingListener().apply {
+                        launcher.execute(it, this)
+                    }.summary
+                }
         val score = 1.0 * summary.testsSucceededCount / summary.testsFoundCount
         val message = "${summary.testsSucceededCount} of ${summary.testsFoundCount} Test cases passed\n" +
                 if (summary.failures.any()) {
