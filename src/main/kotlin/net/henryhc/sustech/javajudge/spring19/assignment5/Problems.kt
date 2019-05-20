85f333aabef8015fae1c968cb12fb391038fb955
Henry Chu <henrytech@outlook.com>
2019-05-20 18:09:34 +0800

    changed grading

--- src/main/kotlin/net/henryhc/sustech/javajudge/spring19/assignment5/Problems.kt
+++ src/main/kotlin/net/henryhc/sustech/javajudge/spring19/assignment5/Problems.kt
@@ -3,14 +3,11 @@ package net.henryhc.sustech.javajudge.spring19.assignment5
 import net.henryhc.sustech.javajudge.*
 import net.henryhc.sustech.javajudge.testcases.CompileTestCase
 
-private val compileProblem = Problem("Compilation", testCasesAverage(
-        CompileTestCase("Gender", setOf("Gender")),
-        CompileTestCase("Customer and Gender", setOf("Customer", "Gender")),
-        CompileTestCase("BankAccount, Customer and Gender", setOf("BankAccount", "Customer", "Gender")),
+private val compileProblem = Problem("Compilation (Not counted for score)", testCasesAverage(
         CompileTestCase("All files", setOf("BankAccount", "BankService", "Customer", "Gender"))
 ))
 
 val problemsAssignment5 = problems(
-        compileProblem worth 30.00,
-        unitTestJudge worth 90.0
+        compileProblem worth 0.00,
+        unitTestJudge worth 120.0
 )
