bffe7a6aedfd5386e9bda5f7f3770e41e6233029
Henry Chu <henrytech@outlook.com>
2019-05-21 10:34:32 +0800

    refactorings

--- src/main/kotlin/net/henryhc/sustech/javajudge/spring19/assignment5/Problems.kt
+++ src/main/kotlin/net/henryhc/sustech/javajudge/spring19/assignment5/Problems.kt
@@ -3,6 +3,7 @@ package net.henryhc.sustech.javajudge.spring19.assignment5
 import net.henryhc.sustech.javajudge.*
 import net.henryhc.sustech.javajudge.testcases.CompileTestCase
 import net.henryhc.sustech.javajudge.testcases.filemarker.FileMarkerTestCase
+import net.henryhc.sustech.javajudge.testcases.unittest.JUnitTestCase
 
 private val compileProblem = Problem("Compilation (Not counted for score)", testCasesAverage(
         CompileTestCase("All files", setOf("BankAccount", "BankService", "Customer", "Gender"))
@@ -12,6 +13,10 @@ private val manualFixPenalty = Problem("Manual fix penalty", testCasesAverage(
         FileMarkerTestCase("Compile fix", "fix",
                 "Fixed compile error", "No fix needed",
                 presentScore = 1.0, notPresentScore = 0.0)
+))
+
+private val unitTestJudge = Problem("Unit test judge", testCases(
+        JUnitTestCase("JUnit provided by Prof. Liu", "A5Test") worth 1.0
 ))
 
 val problemsAssignment5 = problems(
